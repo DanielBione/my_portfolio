33c13e61ec4a4ff573fbba598c377606d0ec7e60
Escolano <escolano@puzzl.com.br>
2024-02-01 06:17:40 -0300

    Adicionando Social

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,10 +13,12 @@ const Footer = () => {
       <div className="footer_socials">
         <a href="https://github.com/DanielBione" target='_blank' rel='noreferrer'><FaGithub /></a>
         <a href="https://www.linkedin.com/in/daniel-escolano-868553252/" target='_blank' rel='noreferrer'><BsLinkedin /></a>
+        <a href="https://www.facebook.com/profile.php?id=100094695927875" target='_blank' rel='noreferrer'><BsFacebook /></a>
+        <a href="https://www.instagram.com/devescolano/" target='_blank' rel='noreferrer'><BsInstagram /></a>
       </div>
 
       <div className="footer__copyright">
-        <small>&copy; Daniel ESCOLANO</small>
+        <small>&copy; Daniel Escolano</small>
       </div>
     </footer>
   )
